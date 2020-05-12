b4db556375546da72d2830bdeac370e3c3a44147
Ben Williams <DeanPBR@gmail.com>
2020-05-12 12:01:24 -0400

    Finished Fortune Teller

--- src/org/wecancodeit/WeCanCodeItFortuneTellerApp.java
+++ src/org/wecancodeit/WeCanCodeItFortuneTellerApp.java
@@ -5,44 +5,96 @@ import java.util.Scanner;
 
 public class WeCanCodeItFortuneTellerApp {
 
-    /*
-     * Part 1
-     DONE - Ask the user for the user’s first name.
-     DONE - Ask the user for the user’s last name.
-    Ask the user for the user’s age.
-    Ask the user for the user’s birth month (as an 'int').
-    Ask the user for the user’s favorite ROYGBIV color.
-        If the user does not know what ROYGBIV is, ask the user to enter “Help” to get a list of the ROYGBIV colors.
-    Ask the user for the user's number of siblings.
-     */
     public static void main(String[] args) {
-	    System.out.println("Hello, welcome to We Can Code IT's Fortune Teller Application!");
+        System.out.println("Hello, welcome to We Can Code IT's Fortune Teller Application!");
 
         Scanner input = new Scanner(System.in);
 
         System.out.println("What is your first name:");
         String firstName = input.nextLine();
-        System.out.println("Your first name is " + firstName);
 
         System.out.println("What is your last name:");
         String lastName = input.nextLine();
-        System.out.println("Your last name is " + lastName);
 
         System.out.println("What is your age:");
         int userAge = input.nextInt();
-        System.out.println("Your age is " + userAge);
 
         System.out.println("What is your birth month number:");
         int userBirthMonth = input.nextInt();
         input.nextLine();
-        System.out.println("Your birth month is " + userBirthMonth);
 
         System.out.println("What is your favorite ROYGBIV color, type HELP if you don't know the colors");
         String favoriteColor = input.nextLine();
-        System.out.println("Your favorite color is " + favoriteColor);
+        if (favoriteColor.equalsIgnoreCase("help")) {
+            System.out.println("ROYGBIV stands for Red, Orange, Yellow, Green, Blue, Indigo, and Violet. Use one of these values.");
+            favoriteColor = input.nextLine();
+        }
 
         System.out.println("How many siblings do you have:");
         int siblingCount = input.nextInt();
-        System.out.println("You have " + siblingCount + " siblings.");
+
+        int yearsToRetirement = 0;
+        if (userAge % 2 == 0) {
+            yearsToRetirement = 12;
+        } else {
+            yearsToRetirement = 14;
+        }
+
+        String vacationHomeLocation = "";
+        if (siblingCount == 0) {
+            vacationHomeLocation = "Boca Raton, FL";
+        } else if (siblingCount == 1) {
+            vacationHomeLocation = "Nassau, Bahamas";
+        } else if (siblingCount == 2) {
+            vacationHomeLocation = "Ponta Negra,Brazil";
+        } else if (siblingCount == 3) {
+            vacationHomeLocation = "Portland, Oregon";
+        } else if (siblingCount > 3) {
+            vacationHomeLocation = "Baton Rouge, LA";
+        } else {
+            vacationHomeLocation = "Chernobyl, Ukraine";
+        }
+
+        String modeOfTransportation = "";
+        switch (favoriteColor.toLowerCase()) {
+            case "red":
+                modeOfTransportation = "Maserati";
+                break;
+            case "orange":
+                modeOfTransportation = "seven chihuahuas pulling a chariot.";
+                break;
+            case "yellow":
+                modeOfTransportation = "school bus";
+                break;
+            case "green":
+                modeOfTransportation = "Tesla";
+                break;
+            case "blue":
+                modeOfTransportation = "Yugo";
+                break;
+            case "indigo":
+                modeOfTransportation = "Honda Civic";
+                break;
+            case "violet":
+                modeOfTransportation = "Mini Cooper";
+                break;
+            default:
+                modeOfTransportation = "taxi cab";
+        }
+
+        double bankBalance = 0.0;
+        if (userBirthMonth > 0 && userBirthMonth <= 4) {
+            bankBalance = 256000.76;
+        } else if (userBirthMonth > 4 && userBirthMonth <= 8) {
+            bankBalance = 3687105.42;
+        }else if (userBirthMonth> 8 && userBirthMonth<=12){
+            bankBalance = 45.65;
+        }
+
+        //[First Name] [Last Name] will retire in [# of years] with [bank balance] in the bank, a vacation home in [location], and travel by [mode of transportation].
+        System.out.println(firstName + " " + lastName + " will retire in " + yearsToRetirement + " years,\n" +
+                "with $" + bankBalance + " in the bank,\n" +
+                "a vacation home in "+ vacationHomeLocation +",\n" +
+                "and travel by "+modeOfTransportation + ".");
     }
 }
