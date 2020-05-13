b80823e93a88f617510df4b94bb3f6cfd129cdb4
Ben Williams <DeanPBR@gmail.com>
2020-05-13 15:31:15 -0400

    Refactoring into methods.

--- src/org/wecancodeit/WeCanCodeItFortuneTellerApp.java
+++ src/org/wecancodeit/WeCanCodeItFortuneTellerApp.java
@@ -10,36 +10,84 @@ public class WeCanCodeItFortuneTellerApp {
 
         Scanner input = new Scanner(System.in);
 
+        String firstName = askForFirstName(input);
+
+        String lastName = askForLastName(input);
+
+        int userAge = askForUserAge(input);
+
+        int userBirthMonth = askForUserBirthMonth(input);
+
+        String favoriteColor = askForFavoriteColor(input);
+
+        int siblingCount = askForSiblingCount(input);
+
+        int yearsToRetirement = determineYearsToRetirement(userAge);
+
+        String vacationHomeLocation = determineVacationHomeLocation(siblingCount);
+
+        String modeOfTransportation = determineModeOfTransportation(favoriteColor);
+
+        double bankBalance = determineBankBalance(userBirthMonth);
+
+        System.out.println(determineFortune(firstName, lastName, yearsToRetirement, vacationHomeLocation, modeOfTransportation, bankBalance));
+    }
+
+    private static String determineFortune(String firstName, String lastName, int yearsToRetirement, String vacationHomeLocation, String modeOfTransportation, double bankBalance) {
+        return firstName + " " + lastName + " will retire in " + yearsToRetirement + " years,\n" +
+                "with $" + bankBalance + " in the bank,\n" +
+                "a vacation home in "+ vacationHomeLocation +",\n" +
+                "and travel by "+modeOfTransportation + ".";
+    }
+
+    private static String askForFirstName(Scanner input) {
         System.out.println("What is your first name:");
-        String firstName = input.nextLine();
+        return input.nextLine();
+    }
 
+    private static String askForLastName(Scanner input) {
         System.out.println("What is your last name:");
-        String lastName = input.nextLine();
+        return input.nextLine();
+    }
 
+    private static int askForUserAge(Scanner input) {
         System.out.println("What is your age:");
-        int userAge = input.nextInt();
+        return input.nextInt();
+    }
 
+    private static int askForUserBirthMonth(Scanner input) {
         System.out.println("What is your birth month number:");
         int userBirthMonth = input.nextInt();
         input.nextLine();
+        return userBirthMonth;
+    }
 
+    private static String askForFavoriteColor(Scanner input) {
         System.out.println("What is your favorite ROYGBIV color, type HELP if you don't know the colors");
         String favoriteColor = input.nextLine();
         if (favoriteColor.equalsIgnoreCase("help")) {
             System.out.println("ROYGBIV stands for Red, Orange, Yellow, Green, Blue, Indigo, and Violet. Use one of these values.");
             favoriteColor = input.nextLine();
         }
+        return favoriteColor;
+    }
 
+    private static int askForSiblingCount(Scanner input) {
         System.out.println("How many siblings do you have:");
-        int siblingCount = input.nextInt();
+        return input.nextInt();
+    }
 
+    private static int determineYearsToRetirement(int userAge) {
         int yearsToRetirement = 0;
         if (userAge % 2 == 0) {
             yearsToRetirement = 12;
         } else {
             yearsToRetirement = 14;
         }
+        return yearsToRetirement;
+    }
 
+    private static String determineVacationHomeLocation(int siblingCount) {
         String vacationHomeLocation = "";
         if (siblingCount == 0) {
             vacationHomeLocation = "Boca Raton, FL";
@@ -54,7 +102,10 @@ public class WeCanCodeItFortuneTellerApp {
         } else {
             vacationHomeLocation = "Chernobyl, Ukraine";
         }
+        return vacationHomeLocation;
+    }
 
+    private static String determineModeOfTransportation(String favoriteColor) {
         String modeOfTransportation = "";
         switch (favoriteColor.toLowerCase()) {
             case "red":
@@ -81,7 +132,10 @@ public class WeCanCodeItFortuneTellerApp {
             default:
                 modeOfTransportation = "taxi cab";
         }
+        return modeOfTransportation;
+    }
 
+    private static double determineBankBalance(int userBirthMonth) {
         double bankBalance = 0.0;
         if (userBirthMonth > 0 && userBirthMonth <= 4) {
             bankBalance = 256000.76;
@@ -90,11 +144,6 @@ public class WeCanCodeItFortuneTellerApp {
         }else if (userBirthMonth> 8 && userBirthMonth<=12){
             bankBalance = 45.65;
         }
-
-        //[First Name] [Last Name] will retire in [# of years] with [bank balance] in the bank, a vacation home in [location], and travel by [mode of transportation].
-        System.out.println(firstName + " " + lastName + " will retire in " + yearsToRetirement + " years,\n" +
-                "with $" + bankBalance + " in the bank,\n" +
-                "a vacation home in "+ vacationHomeLocation +",\n" +
-                "and travel by "+modeOfTransportation + ".");
+        return bankBalance;
     }
 }
